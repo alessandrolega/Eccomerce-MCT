27f88bb4844018d70edcc08f373a98176abe285b
OleksandrOleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-03-10 01:01:41 +0100

    calculation of the percentage of reviews

--- views/reviews/js/ReviewsController.js
+++ views/reviews/js/ReviewsController.js
@@ -10,14 +10,14 @@ export default class ReviewsController {
     try {
       const result = await fetch(`${this.Utils.viewsPath}reviews/js/data.json`);
       const data = await result.json();
-      return this.renderReviews(data);
+      this.renderReviews(data);
+      this.counterStar(data);
     } catch (error) {
       console.error(error);
     }
   }
 
   renderReviews(data) {
-    console.log(data);
     const reviewsContainer = document.querySelector("#reviewsContainer");
     const html = data
       .map((d) => {
@@ -46,5 +46,38 @@ export default class ReviewsController {
       })
       .join("");
     reviewsContainer.insertAdjacentHTML("afterbegin", html);
+  }
+  counterStar(data) {
+    const allSvg = { 5: 0, 4: 0, 3: 0, 2: 0, 1: 0 };
+    data.forEach((d) => {
+      for (const key in allSvg) {
+        if (+key === d.star) allSvg[+key] += 1;
+      }
+    });
+
+    const containerStar = document.querySelector("#containerStar");
+    const resultStar = Object.values(allSvg);
+    const sumStar = resultStar.reduce((count, s) => (count += s), 0);
+    resultStar.forEach((star, i) => {
+      const html = `
+      <div class="flex items-center gap-1">
+          <div class="flex items-center gap-1">
+              <p>${i + 1}</p><img class="w-5 h-5" src="${
+        this.Utils.assetsPath
+      }images/svg/star.svg" /></div>
+              <div class="w-52 bg-gray-200 rounded-full h-2.5 flex">
+                  <div class="bg-pink-600 h-2.5 rounded-full" style="width: ${
+                    (100 / sumStar) * star
+                  }%"></div>
+          </div>
+          <div>${Math.trunc((100 / sumStar) * star)}%</div>
+      </div>`;
+      containerStar.insertAdjacentHTML("afterbegin", html);
+    });
+
+    // const mediaStar =
+    //   resultStar.reduce((count, s, i) => (count += s * (i + 1)), 0) / sumStar;
+    // const pippoStar = [...document.querySelectorAll("img.pippo")];
+    // pippoStar[pippoStar.length - 1].style.cssText = 'fill: 50%';
   }
 }
--- views/reviews/reviewsViews.php
+++ views/reviews/reviewsViews.php
@@ -5,26 +5,11 @@
             <h2 class="titleH2">Cosa pensano di noi</h2>
             <div class="flex items-center mb-2">
                 <?php for ($i = 0; $i < 5; $i++) {
-                    echo '<img class="w-5 h-5" src="' . $svgPath . '" />';
+                    echo '<img class="w-5 h-5 pippo" src="' . $svgPath . '" />';
                 } ?>
                 <p class="ml-2 text-sm font-medium">5 su 5</p>
             </div>
-            <div class="mb-5">
-                <?php
-                $randomNumber = [91, 3, 2, 2, 2];
-                foreach ($randomNumber as $key => $value) {
-                    echo '
-                            <div class="flex items-center gap-1">
-                                    <div class="flex items-center gap-1">
-                                        <p>' . (count($randomNumber) - $key) . '</p><img class="w-5 h-5" src="' . $svgPath . '" /></div>
-                                    <div class="w-52 bg-gray-200 rounded-full h-2.5 flex">
-                                        <div class="bg-pink-600 h-2.5 rounded-full" style="width: ' . $value . '%"></div>
-                                    </div>
-                                <div>' . $value . '%</div>
-                            </div>';
-                }
-                ?>
-            </div>
+            <div id="containerStar" class="mb-5"></div>
             <button type="button" class="text-white bg-emerald-700 hover:text-white hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
                 Scrivi una recensione
                 <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
